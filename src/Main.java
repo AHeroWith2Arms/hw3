83393739ec11328abf58113fe8b24116cb065a05
AHeroWith2Arms <kirillvalentinka@gmail.com>
2025-02-19 16:12:08 +0300

    new p

--- src/Main.java
+++ src/Main.java
@@ -1,96 +1,53 @@
 public class Main {
     public static void main(String[] args) {
-        //это все мне C++ напоминает... Страшное время было хотя синтаксис почти такой же как и на плюсах
-        int iVal = 569;
-        byte bVal = 67;
-        short sVal = 27897;
-        long lVal = 987678965549L;
-        float fVal = 27.12f;
-        double dVal = 2.786;
+        int age = 20;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать");
+        }
 
-        System.out.println("Значение переменной iVal с типом int равно " + iVal);
-        System.out.println("Значение переменной bVal с типом byte равно " + bVal);
-        System.out.println("Значение переменной sVal с типом short равно " + sVal);
-        System.out.println("Значение переменной lVal с типом long равно " + lVal);
-        System.out.println("Значение переменной fVal с типом float равно " + fVal);
-        System.out.println("Значение переменной dVal с типом double равно " + dVal);
+        int temp = 4;
+        if (temp < 5) {
+            System.out.println("На улице " + temp + " градусов, нужно надеть шапку");
+        } else {
+            System.out.println("На улице " + temp + " градусов, можно идти без шапки");
+        }
 
-        int lS = 23;
-        int aS = 27;
-        int eS = 30;
-        int tS = lS + aS + eS;
-        int tP = 480;
-        int pPS = tP / tS;
+        int speed = 70;
+        if (speed > 60) {System.out.println("Если скорость " + speed + ", то придется заплатить штраф");} else {System.out.println("Если скорость " + speed + ", то можно ездить спокойно");}
 
-        System.out.println("На каждого ученика рассчитано " + pPS + " листов бумаги");
+        int age2 = 21;
+        if (age2 >= 2 && age2 <= 6) {System.out.println("Если возраст человека равен " + age2 + ", то ему нужно ходить в детский сад");} else if (age2 >= 7 && age2 <= 17) {
+            System.out.println("Если возраст человека равен " + age2 + ", то ему нужно ходить в школу");} else if (age2 >= 18 && age2 <= 24) {
+            System.out.println("Если возраст человека равен " + age2 + ", то ему нужно ходить в университет");} else {
+            System.out.println("Если возраст человека равен " + age2 + ", то ему пора ходить на работу");}
 
-        int bPM = 16;
-        int m20 = 20;
-        int b20M = (bPM / 2) * m20;
-        System.out.println("За 20 машина произвела " + b20M + " штук бутылок");
+        int childAge = 10;
+        if (childAge < 5) {
+            System.out.println("Если возраст ребенка равен " + childAge + ", то ему нельзя кататься на аттракционе");
+        } else if (childAge < 14) {
+            System.out.println("Если возраст ребенка равен " + childAge + ", то ему можно кататься на аттракционе в сопровождении взрослого");
+        } else {
+            System.out.println("Если возраст ребенка равен " + childAge + ", то ему можно кататься на аттракционе без сопровождения взрослого");
+        }
 
-        int mD = 1440;
-        int bD = (bPM / 2) * mD;
-        System.out.println("За 1 сутки машина произвела " + bD + " штук бутылок");
+        int seats = 50;
+        if (seats < 60) {
+            System.out.println("Место в вагоне есть: сидячие/стоячие");
+        } else if (seats < 102) {
+            System.out.println("Место в вагоне есть: стоячие");
+        } else {
+            System.out.println("Вагон уже полностью забит");
+        }
 
-        int d3 = 3;
-        int b3D = bD * d3;
-        System.out.println("За 3 дня машина произвела " + b3D + " штук бутылок");
-
-        int dM = 30;
-        int bM = bD * dM;
-        System.out.println("За 1 месяц машина произвела " + bM + " штук бутылок");
-        int cL = 120;
-        int bP = 2;
-        int kP = 4;
-        int cC = cL / (bP + kP);
-        int bC = cC * bP;
-        int kC = cC * kP;
-
-        System.out.println("В школе, где " + cC + " классов, нужно " + bC + " банок белой краски и " + kC + " банок коричневой краски");
-
-        int bN = 5 * 80;
-        int mL = 200 / 100 * 105;
-        int iC = 2 * 100;
-        int eG = 4 * 70;
-        int tW = bN + mL + iC + eG;
-        double tWk = tW / 1000.0;
-
-        System.out.println("Вес спортзавтрака: " + tW + " грамм, " + tWk + " килограмм");
-
-        int wL = 7000;
-        int pDMin = 250;
-        int pDMax = 500;
-        int dMin = wL / pDMax;
-        int dMax = wL / pDMin;
-        int avgD = (dMin + dMax) / 2;
-
-        System.out.println("Дней на похудение при потере 250 грамм в день: " + dMin);
-        System.out.println("Дней на похудение при потере 500 грамм в день: " + dMax);
-        System.out.println("Среднее количество дней: " + avgD);
-
-        double sM = 67760;
-        double sD = 83690;
-        double sK = 76230;
-
-        double nSM = sM * 1.1;
-        double nSD = sD * 1.1;
-        double nSK = sK * 1.1;
-
-        double yIM = sM * 12;
-        double yID = sD * 12;
-        double yIK = sK * 12;
-
-        double nYIM = nSM * 12;
-        double nYID = nSD * 12;
-        double nYIK = nSK * 12;
-
-        double dIM = nYIM - yIM;
-        double dID = nYID - yID;
-        double dIK = nYIK - yIK;
-
-        System.out.println("Маша теперь получает " + nSM + " рублей. Годовой доход вырос на " + dIM + " рублей");
-        System.out.println("Денис теперь получает " + nSD + " рублей. Годовой доход вырос на " + dID + " рублей");
-        System.out.println("Кристина теперь получает " + nSK + " рублей. Годовой доход вырос на " + dIK + " рублей");
+        int one = 5, two = 10, three = 7;
+        if (one >= two && one >= three) {
+            System.out.println("Наибольшее число: " + one);
+        } else if (two >= one && two >= three) {
+            System.out.println("Наибольшее число: " + two);
+        } else {
+            System.out.println("Наибольшее число: " + three);
+        }
     }
 }
